8eea9edb5a42c37923c9d07be5087df52b3b7cd5
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-22 12:54:44 -0400

    MVP complete. dotenv config added to index for heroku deployment.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+require("dotenv").config();
 const server = require("./server.js");
 
-const port = 5000 || process.env.PORT;
-const greeting = "GREETINGS" || process.env.GREETING;
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
 
 server.listen(port, () => {
   console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
